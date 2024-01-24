c00e6c8e957f531aca4142818cea50743ac4e4d7
Joon-Erik Sæther <54800950+joonerik@users.noreply.github.com>
2024-01-24 11:29:24 +0100

    Porto trajectories and dtw benchmark structuring (#1)
    * include meta/index files for rome dataset while gitignoring txt trajectories
    
    * support trajectory saving for porto and rome, and set limit to 3000 trajectories
    
    * create index/meta files for up to 3000 trajectories while gitignoring actual trajectory files
    
    * structure dtw benchmark creation

--- processing/utils/save_trajectory.py
+++ processing/utils/save_trajectory.py
@@ -3,11 +3,14 @@
 
 # Save function
 def save_current_trajectory(
-    OUTPUT_FOLDER: str, file_name: str, trajectory: list[tuple[float]]
+    OUTPUT_FOLDER: str,
+    file_name: str,
+    trajectory: list[tuple[float]],
+    split_coordinate: bool,
 ) -> None:
     with open(f"{OUTPUT_FOLDER}/R_{file_name}.txt", "w") as file:
         for coordinate in trajectory:
-            lat, lon = coordinate
+            lat, lon = coordinate.split(",") if split_coordinate else coordinate
             file.write("%s, %s\n" % (lat, lon))
         file.close()
         return
