10f3357165450d7acdda42b0db28440bc60c3308
joonerik <joon-erik@hotmail.com>
2024-01-31 16:59:05 +0100

    change prefix for output files and trajectories

--- utils/helpers/save_trajectory.py
+++ utils/helpers/save_trajectory.py
@@ -6,8 +6,9 @@ def save_current_trajectory(
     OUTPUT_FOLDER: str,
     file_name: str,
     trajectory: list[tuple[float]],
+    trajectory_file_prefix: str = "R",
 ) -> None:
-    with open(f"{OUTPUT_FOLDER}/R_{file_name}.txt", "w") as file:
+    with open(f"{OUTPUT_FOLDER}/{trajectory_file_prefix}_{file_name}.txt", "w") as file:
         for coordinate in trajectory:
             lat, lon = coordinate
             file.write("%s, %s\n" % (lat, lon))
